87773b193d9858157da88c07916ba0291320e28c
Trong <nguyenhuytronglqd2007@gmail.com>
2025-09-21 12:33:21 -0500

    build UI frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import AddTask from '@/components/AddTask';
+import Header from '@/components/Header';
+import TaskList from '@/components/TaskList';
+import TaskListPagination from '@/components/TaskListPagination';
+import DateTimeFilter from '@/components/DateTimeFilter';
+import Footer from '@/components/Footer';
+import StatsAndFilters from '@/components/StatsAndFilters';
+import React from 'react';
 
 const HomePage = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <div className="container pt-8 mx-auto">
+      <div className="w-full max-w-2xl mx-auto space-y-6">
+        {/* Header Section*/}
+        <Header />
 
-export default HomePage
+        {/* Create Task */}
+        <AddTask />
+
+        {/*Stats and Filters*/}
+        <StatsAndFilters />
+
+        {/* Task List */}
+        <TaskList />
+
+        {/*Date time filter*/}
+        <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+          <TaskListPagination />
+          <DateTimeFilter />
+        </div>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-slate-50">
+      <img
+        src="404_NotFound.png"
+        alt="not found"
+        className="max-w-full mb-6 w-96"
+      />
 
-export default NotFound
+      <p className="text-xl font-semibold">404 - Page Not Found </p>
+
+      <a
+        href="/"
+        className="inline-block px-6 py-3 mt-6 font-medium text-white transition shadow-md
+        bg-primary rounded-2xl hover:bg-primary-dark"
+      >
+        Go to Homepage
+      </a>
+    </div>
+  );
+};
+
+export default NotFound;
