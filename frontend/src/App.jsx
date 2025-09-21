44e150204b2783c3c2fa82ba64c2cbfa3221e1fa
Trong <nguyenhuytronglqd2007@gmail.com>
2025-09-21 10:59:26 -0500

    update frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import {Toaster} from 'sonner';
+import {BrowserRouter, Routes, Route} from 'react-router';
+import HomePage from './pages/HomePage';
+import NotFound from './pages/NotFound';
+
+
+function App() {
+
+
+  return (
+    <>
+
+    <Toaster />
+
+
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
+
+
+
+    </>
+  )
+}
+
+export default App
