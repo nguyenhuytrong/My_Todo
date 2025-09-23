b02fc9d4969a7c1016b832fd7f1fce627b33e7c0
Trong <nguyenhuytronglqd2007@gmail.com>
2025-09-22 22:53:09 -0500

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,29 @@ import taskRoute from './routes/tasksRouters.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({ origin: '*' }));
+}
 
 app.use('/api/tasks', taskRoute);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
